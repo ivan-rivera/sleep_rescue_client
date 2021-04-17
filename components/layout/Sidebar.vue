045600a034626b9afd9f6a95cc95a19c94093daf
ivan.s.rivera@gmail.com <ivan.s.rivera@gmail.com>
2021-04-17 20:41:17 +0200

    sidebar animations ready

--- components/layout/Sidebar.vue
+++ components/layout/Sidebar.vue
@@ -1,45 +1,92 @@
 <template>
-  <div class="sr-sidebar sr-sidebar-default">
-    <div class="mt-10 w-full cursor-pointer" @click="expand">
-      <div>
-        <font-awesome-icon :icon="['fa', 'bars']" />
+  <div
+    class="sr-sidebar slide-in"
+    :class="expanded ? 'sr-sidebar-full' : 'sr-sidebar-default'"
+  >
+    <div
+      :class="{ 'menu-border': expanded }"
+      class="mt-10 w-full cursor-pointer"
+      @click="expand"
+    >
+      <div class="option-holder">
+        <div class="icon-box">
+          <font-awesome-icon :icon="['fa', expanded ? 'times' : 'bars']" />
+        </div>
+        <transition name="fade">
+          <p v-if="expanded" class="text-3xl pl-2 uppercase">Menu</p>
+        </transition>
       </div>
     </div>
     <div class="flex flex-col justify-between h-72">
       <NuxtLink to="/dashboard">
-        <div :class="isSelected('dashboard')">
-          <font-awesome-icon :icon="['fa', 'chart-pie']" />
+        <div class="option-holder" :class="isSelected('dashboard')">
+          <div class="icon-box">
+            <font-awesome-icon :icon="['fa', 'chart-pie']" />
+          </div>
+          <transition name="fade">
+            <p v-if="expanded" class="route-desc">Dashboard</p>
+          </transition>
         </div>
       </NuxtLink>
       <NuxtLink to="/goals">
-        <div :class="isSelected('goals')">
-          <font-awesome-icon :icon="['fa', 'trophy']" />
+        <div class="option-holder" :class="isSelected('goals')">
+          <div class="icon-box">
+            <font-awesome-icon :icon="['fa', 'trophy']" />
+          </div>
+          <transition name="fade">
+            <p v-if="expanded" class="route-desc">Goals</p>
+          </transition>
         </div>
       </NuxtLink>
       <NuxtLink to="/thoughts">
-        <div :class="isSelected('thoughts')">
-          <font-awesome-icon :icon="['fa', 'brain']" />
+        <div class="option-holder" :class="isSelected('thoughts')">
+          <div class="icon-box">
+            <font-awesome-icon :icon="['fa', 'comment-dots']" />
+          </div>
+          <transition name="fade">
+            <p v-if="expanded" class="route-desc">Thought Checker</p>
+          </transition>
         </div>
       </NuxtLink>
       <NuxtLink to="/isi">
-        <div :class="isSelected('isi')">
-          <font-awesome-icon :icon="['fa', 'procedures']" />
+        <div class="option-holder" :class="isSelected('isi')">
+          <div class="icon-box">
+            <font-awesome-icon :icon="['fa', 'clipboard-list']" />
+          </div>
+          <transition name="fade">
+            <p v-if="expanded" class="route-desc">ISI Survey</p>
+          </transition>
         </div>
       </NuxtLink>
       <NuxtLink to="/learn">
-        <div :class="isSelected('learn')">
-          <font-awesome-icon :icon="['fa', 'book-medical']" />
+        <div class="option-holder" :class="isSelected('learn')">
+          <div class="icon-box">
+            <font-awesome-icon :icon="['fa', 'book-medical']" />
+          </div>
+          <transition name="fade">
+            <p v-if="expanded" class="route-desc">Learning Center</p>
+          </transition>
         </div>
       </NuxtLink>
     </div>
     <div class="mb-10">
       <NuxtLink to="/account">
         <div class="option-holder" :class="isSelected('account')">
-          <font-awesome-icon :icon="['fa', 'user-circle']" />
+          <div class="icon-box">
+            <font-awesome-icon :icon="['fa', 'user-circle']" />
+          </div>
+          <transition name="fade">
+            <p v-if="expanded" class="route-desc">Account</p>
+          </transition>
         </div>
       </NuxtLink>
-      <div class="cursor-pointer" @click="$auth.logout()">
-        <font-awesome-icon :icon="['fa', 'door-open']" />
+      <div class="cursor-pointer option-holder" @click="$auth.logout()">
+        <div class="icon-box">
+          <font-awesome-icon :icon="['fa', 'door-open']" />
+        </div>
+        <transition name="fade">
+          <p v-if="expanded" class="route-desc">Log out</p>
+        </transition>
       </div>
     </div>
   </div>
@@ -47,14 +94,12 @@
 
 <script>
 export default {
-  // TODO: expand logic
-  // TODO: highlight current (use sexy curve outward stuff)
-  // TODO: hover effects + tooltip
   // TODO: transition effects
-  // TODO: move CSS into classes
+  // TODO: hover effects + tooltip
+  transition: 'fade',
   data() {
     return {
-      expanded: false,
+      expanded: true,
     }
   },
   mounted() {
@@ -66,38 +111,69 @@ export default {
       return this.$route.name === page ? 'selected-page' : ''
     },
     expand() {
-      console.log('...')
+      this.expanded = !this.expanded
     },
   },
 }
 </script>
 
-<style>
+<style scoped>
+.fade-enter-active {
+  transition: opacity 0.5s;
+  transition-delay: 0.5s;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+
 .sr-sidebar {
   @apply fixed
   left-0 top-0
   h-full
   flex flex-col justify-between
-  text-dark text-center text-3xl
-  bg-white;
+  text-dark text-3xl
+  text-left
+  bg-white
+  border-r-2 border-dark;
+  transition: width 500ms;
 }
 
 .sr-sidebar-default {
-  @apply w-16;
+  @apply w-20;
 }
 
 .sr-sidebar-full {
   @apply w-72;
+  /*  72*/
 }
 
 .selected-page {
   @apply bg-dark text-secondary text-4xl
   rounded-bl-3xl rounded-tl-3xl
-  pt-4 pb-4;
+  pl-4 pt-4 pb-4
+  text-center;
+}
+
+.icon-box {
+  @apply w-12;
 }
 
 .option-holder {
-  @apply pt-2 pb-2;
+  @apply pt-2 pb-2 pl-6;
   @apply mt-2 mb-2;
+  @apply flex flex-row items-center;
+}
+
+.route-desc {
+  @apply pl-2
+  uppercase
+  text-base font-bold
+  overflow-hidden whitespace-nowrap;
+}
+
+.menu-border {
+  @apply border-t-2 border-b-2 border-dark;
 }
 </style>
